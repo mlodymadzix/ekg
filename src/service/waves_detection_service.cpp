83d517175d3def93ecfabe0db9d50f945de0f2d4
mlodymadzix <madzia.suchan.15@interia.eu>
2025-11-11 14:06:27 +0100

    Update waves_detection_service.cpp

--- src/service/waves_detection_service.cpp
+++ src/service/waves_detection_service.cpp
@@ -2,6 +2,6 @@
 
 std::vector<WaveAnnotatedSignalDatapoint> WavesDetectionService::Detect(const std::vector<SignalDatapoint> &datapoints,
                                                                         int frequency) {
-    // TODO(Magda): trzeba uzupełnić ok
+    // TODO(Magda): trzeba uzupełnić
     return std::vector<WaveAnnotatedSignalDatapoint>{};
 }
